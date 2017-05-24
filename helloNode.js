e585ac485f41c58a1233094504d5497c0f00906d
Andrew Smith <andy-smith@comcast.net>
2017-05-24 15:14:50 -0400

    first commit

---
+++ helloNode.js
@@ -0,0 +1,17 @@
+var express = require('express');
+app = express();
+
+app.get('/hello', function(req, res) {
+	res.send("Hello...");
+});
+app.get('/node', function(req, res) {
+        res.send("...Node!");
+});
+app.get('/', function(req, res) {
+        res.send("Hello Node!");
+});
+
+app.listen(8080, function() {
+	console.log("Spinning up on port 8080...");
+});
+
